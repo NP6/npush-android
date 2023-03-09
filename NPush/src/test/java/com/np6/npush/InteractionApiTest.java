ba5585e2b2282a7b1ca6dbae6b2b462a5f6e352c
bisenbrandt <baptiste.isenbrandt@gmail.com>
2023-03-09 14:48:07 +0100

    add InteractionApiTest + rename tests for SubscriptionApi

--- NPush/src/test/java/com/np6/npush/InteractionApiTest.java
+++ NPush/src/test/java/com/np6/npush/InteractionApiTest.java
@@ -14,6 +14,7 @@ import static okhttp3.mock.MediaTypes.MEDIATYPE_JSON;
 import androidx.test.ext.junit.runners.AndroidJUnit4;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
+import com.np6.npush.internal.api.InteractionApi;
 import com.np6.npush.internal.api.SubscriptionApi;
 import com.np6.npush.internal.core.Constants;
 import com.np6.npush.internal.core.concurrency.Concurrent;
@@ -45,7 +46,7 @@ import okhttp3.mock.Rule;
 
 @Config(sdk = {30})
 @RunWith(AndroidJUnit4.class)
-public class SubscriptionApiTest {
+public class InteractionApiTest {
 
 
     @Before
@@ -53,69 +54,49 @@ public class SubscriptionApiTest {
 
     }
 
-    @Test(expected = IllegalArgumentException.class)
-    public void initializeSubscriptionApiWithoutIdentity() {
-        OkHttpClient client = new OkHttpClient.Builder()
-                .build();
-
-        SubscriptionApi.create(null);
-    }
-
     @Test
-    public void testCallForPutSubscriptionWithBadRequestHttpResponse() throws InterruptedException, JsonProcessingException, ExecutionException {
+    public void testCallForTrackInteractionWithBadRequestHttpResponse() throws InterruptedException, JsonProcessingException, ExecutionException {
         MockInterceptor interceptor = new MockInterceptor();
 
         interceptor.addRule(new Rule.Builder()
-                .put()
+                .get()
                 .respond(HTTP_400_BAD_REQUEST));
 
         OkHttpClient client = new OkHttpClient.Builder()
                 .addInterceptor(interceptor)
                 .build();
 
-        SubscriptionApi api = new SubscriptionApi("MCOM032", new Driver(client));
+        InteractionApi api = new InteractionApi( new Driver(client));
 
-        // Fake subscription
-        Subscription subscription = new Subscription()
-                .setId(UUID.randomUUID())
-                .setGateway(new Firebase("testtoken"));
-
-        Response response = api.put(subscription).get();
+        Response response = api.get("https://tracking.np6.com", "ze9f1ez8f19rz8e98ze4r89e9g84te79r74tr").get();
 
         assertFalse(response.isSuccessful());
         assertFalse(response.isRedirect());
         assertEquals(400, response.code());
-        assertEquals(response.request().method(), "PUT");
-        assertTrue(response.request().url().isHttps());
+        assertEquals(response.request().method(), "GET");
 
     }
 
     @Test
-    public void testCallForPutSubscriptionWithOkHttpResponse() throws InterruptedException, ExecutionException {
+    public void testCallForTrackInteractionWithOkHttpResponse() throws InterruptedException, ExecutionException {
         MockInterceptor interceptor = new MockInterceptor();
 
         interceptor.addRule(new Rule.Builder()
-                .put()
+                .get()
                 .respond(HttpCode.HTTP_200_OK));
 
         OkHttpClient client = new OkHttpClient.Builder()
                 .addInterceptor(interceptor)
                 .build();
 
-        SubscriptionApi api = new SubscriptionApi("MCOM032", new Driver(client));
+        InteractionApi api = new InteractionApi( new Driver(client));
 
-        // Fake subscription
-        Subscription subscription = new Subscription()
-                .setId(UUID.randomUUID())
-                .setGateway(new Firebase("testtoken"));
-
-        Response response = api.put(subscription).get();
+        Response response = api.get("https://tracking.np6.com", "ze9f1ez8f19rz8e98ze4r89e9g84te79r74tr").get();
 
         assertTrue(response.isSuccessful());
         assertFalse(response.isRedirect());
         assertEquals(200, response.code());
-        assertEquals(response.request().method(), "PUT");
-        assertTrue(response.request().url().isHttps());
+        assertEquals(response.request().method(), "GET");
 
     }
 
