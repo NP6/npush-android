c4e9e8b602a51dcff63af972095e32353c436d20
whiletrues <baptiste.isenbrandt@gmail.com>
2023-04-06 18:46:36 +0200

    Application activity clean

--- demo/src/main/java/com/np6/demo/MainActivity.java
+++ demo/src/main/java/com/np6/demo/MainActivity.java
@@ -2,9 +2,6 @@ package com.np6.demo;
 
 import android.app.NotificationChannel;
 import android.app.NotificationManager;
-import android.app.TaskStackBuilder;
-import android.content.Intent;
-import android.net.Uri;
 import android.os.Build;
 import android.os.Bundle;
 
@@ -22,7 +19,6 @@ import androidx.navigation.ui.NavigationUI;
 import com.np6.demo.databinding.ActivityMainBinding;
 import com.np6.npush.Config;
 import com.np6.npush.NPush;
-import com.np6.npush.internal.models.contact.ContactHash;
 import com.np6.npush.internal.models.contact.ContactId;
 
 import java.util.UUID;
@@ -32,10 +28,10 @@ public class MainActivity extends AppCompatActivity {
     private AppBarConfiguration appBarConfiguration;
     private ActivityMainBinding binding;
 
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
 
         String marketingChannel = "marketingChannel";
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
@@ -61,29 +57,6 @@ public class MainActivity extends AppCompatActivity {
 
         NPush.Instance().initialize(this);
 
-        NPush.Instance().setInterceptor((context, deeplink) -> {
-            TaskStackBuilder stackBuilder = TaskStackBuilder.create(context);
-
-            Intent intentMain = new Intent(context, MainActivity.class);
-            stackBuilder.addNextIntent(intentMain);
-
-            Intent intent = new Intent(context, ProductsActivity.class);
-            stackBuilder.addNextIntent(intent);
-
-            Uri uri = Uri.parse(deeplink);
-            Intent deeplinkIntent = new Intent(Intent.ACTION_VIEW)
-                    .addFlags(Intent.FLAG_ACTIVITY_NEW_TASK)
-                    .setData(uri);
-            stackBuilder.addNextIntent(deeplinkIntent);
-            return stackBuilder;
-        });
-
-        try {
-            Thread.sleep(800);
-        } catch (InterruptedException e) {
-            e.printStackTrace();
-        }
-
         NPush.Instance().setContact(this, new ContactId("000T39KL"));
 
         binding = ActivityMainBinding.inflate(getLayoutInflater());
@@ -91,22 +64,9 @@ public class MainActivity extends AppCompatActivity {
 
         setSupportActionBar(binding.toolbar);
 
-        NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_content);
+        NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_content_main);
         appBarConfiguration = new AppBarConfiguration.Builder(navController.getGraph()).build();
         NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
-
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
-            String CHANNEL_ID = "Customer channel id";
-            CharSequence name = "Customer channel name";
-            String description = "Customer channel description";
-            int importance = NotificationManager.IMPORTANCE_DEFAULT;
-            NotificationChannel channel = new NotificationChannel(CHANNEL_ID, name, importance);
-            channel.setDescription(description);
-            // Register the channel with the system; you can't change the importance
-            // or other notification behaviors after this
-            NotificationManager notificationManager = getSystemService(NotificationManager.class);
-            notificationManager.createNotificationChannel(channel);
-        }
 
         binding.fab.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -117,10 +77,9 @@ public class MainActivity extends AppCompatActivity {
         });
     }
 
-
     @Override
     public boolean onSupportNavigateUp() {
-        NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_content);
+        NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_content_main);
         return NavigationUI.navigateUp(navController, appBarConfiguration)
                 || super.onSupportNavigateUp();
     }
--- demo/src/main/java/com/np6/demo/SecondFragment.java
+++ demo/src/main/java/com/np6/demo/SecondFragment.java
@@ -1,6 +1,5 @@
 package com.np6.demo;
 
-import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -9,11 +8,12 @@ import android.view.ViewGroup;
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 import androidx.navigation.fragment.NavHostFragment;
-import com.np6.demo.databinding.FragmentProduct1Binding;
 
-public class Product1Fragment extends Fragment {
+import com.np6.demo.databinding.FragmentSecondBinding;
 
-private FragmentProduct1Binding binding;
+public class SecondFragment extends Fragment {
+
+    private FragmentSecondBinding binding;
 
     @Override
     public View onCreateView(
@@ -21,24 +21,24 @@ private FragmentProduct1Binding binding;
             Bundle savedInstanceState
     ) {
 
-      binding = FragmentProduct1Binding.inflate(inflater, container, false);
-      return binding.getRoot();
+        binding = FragmentSecondBinding.inflate(inflater, container, false);
+        return binding.getRoot();
 
     }
 
     public void onViewCreated(@NonNull View view, Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
 
-        binding.button.setOnClickListener(new View.OnClickListener() {
+        binding.buttonSecond.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Intent intent = new Intent(getActivity().getApplication(),ProductsActivity.class);
-                startActivity(intent);
+                NavHostFragment.findNavController(SecondFragment.this)
+                        .navigate(R.id.action_SecondFragment_to_FirstFragment);
             }
         });
     }
 
-@Override
+    @Override
     public void onDestroyView() {
         super.onDestroyView();
         binding = null;
